a2e8d980a44e460e3a16a9b07cf02cb7f825ad39
muni91 <ronidod910@gmail.com>
2020-10-17 22:25:52 +0530

    complete skeleton of tweet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+function Tweet(){
+    return(
+        <div className='tweet'>
+            <Avatar/>
+            <div className="content">
+                <NameWithHandle/><Time/>
+                <Message/>
+            </div>
+            <div className="buttons">
+                <ReplayButton/>
+                <RetweetButton/>
+                <LikeButton/>
+                <MoreOptionsButton/>
+            </div>
+        </div>
+    );
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Tweet/>,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Message(){
+    return(
+        <div className='message'>
+            this is message
+        </div>
+    );
+}
+function Avatar(){
+    return(
+        <img 
+        src="https://www.gavatar.com/avatar/nothing"
+        className="avatar"
+        alt="avatar"/>
+    );
+}
+function NameWithHandle(){
+    return(
+        <span className="name-with-handle">
+            <span className="name">Your Name</span>
+            <span className="handle">@yourhandle</span>
+        </span>
+    );
+}
+const Time =() =>(
+    <span className="time">3h ago</span>
+);
+const ReplayButton =()=>(
+    <i className="fa fa-replay-button"/>
+);
+const RetweetButton=()=>(
+    <i className="fa fa-retweet retweet-button"/>
+);
+const LikeButton=()=>(
+    <i className="fa fa-heart like-button"/>
+);
+const MoreOptionsButton=()=>(
+    <i className="fa fa-ellipsis-h more-options-buttton"/>
+);
